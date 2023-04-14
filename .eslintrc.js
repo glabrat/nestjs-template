2244f62f804d127149f11ca5f72c05166ba34f15
glabrat <german.labra@drimo.com>
2023-04-14 12:24:49 -0400

    feat: add github pull request template and fix eslint configuration vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'rokket-labs',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
